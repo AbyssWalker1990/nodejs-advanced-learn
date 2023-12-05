43f2b0e2c35eed0abc57115ed51a582e8474ed37
bordiian <volodymyr_bordiyan@dev.abnk.uk>
2023-12-05 18:29:12 +0200

    add simple redis caching example

--- advancedNodeStart/routes/blogRoutes.js
+++ advancedNodeStart/routes/blogRoutes.js
@@ -4,55 +4,54 @@ const util = require('util')
 
 const Blog = mongoose.model('Blog')
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
+module.exports = (app) => {
+    app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+        const blog = await Blog.findOne({
+            _user: req.user.id,
+            _id: req.params.id,
+        })
+
+        res.send(blog)
     })
 
-    res.send(blog)
-  })
+    app.get('/api/blogs', requireLogin, async (req, res) => {
+        const redis = require('redis')
+        const client = redis.createClient({
+            host: 'localhost',
+            port: 6379,
+        })
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis')
-    const client = redis.createClient({
-      host: 'localhost',
-      port: 6379
+        client.get = util.promisify(client.get)
+        const cachedBlogs = await client.get(req.user.id)
+
+        if (cachedBlogs) {
+            console.log('Serving from cache')
+            return res.send(JSON.parse(cachedBlogs))
+        }
+
+        const blogs = await Blog.find({ _user: req.user.id })
+        console.log('Serving from MongoDB')
+        res.send(blogs)
+        await client.set(req.user.id, JSON.stringify(blogs))
     })
 
-    client.get = util.promisify(client.get)
-    console.log('Serving from cache')
-    const cachedBlogs = await client.get(req.user.id)
-    console.log('cachedBlocks: ', cachedBlogs)
+    app.post('/api/blogs', requireLogin, async (req, res) => {
+        const { title, content } = req.body
 
-    const blogs = await Blog.find({ _user: req.user.id })
-    console.log('Serving from MongoDB')
-    res.send(blogs)
-    await client.set(req.user.id, JSON.stringify(blogs))
+        const blog = new Blog({
+            title,
+            content,
+            _user: req.user.id,
+        })
 
-  })
-
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body
-
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
+        try {
+            await blog.save()
+            res.send(blog)
+        } catch (err) {
+            res.send(400, err)
+        }
     })
-
-    try {
-      await blog.save()
-      res.send(blog)
-    } catch (err) {
-      res.send(400, err)
-    }
-  })
 }
-
-
-
 
 // const redis = require('redis')
 // const client = redis.createClient({
