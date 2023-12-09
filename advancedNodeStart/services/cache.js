0cc944a24e57f7d4aad8037fcf8c243db189a39a
Vova <bordiian.v@gmail.com>
2023-12-09 14:31:28 +0200

    make cache logig toggleable

--- advancedNodeStart/services/cache.js
+++ advancedNodeStart/services/cache.js
@@ -10,22 +10,31 @@ client.get = util.promisify(client.get)
 
 const exec = mongoose.Query.prototype.exec
 
+mongoose.Query.prototype.cache = function () {
+    this.useCache = true
+    return this
+}
+
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments)
+    }
+
     const assignedHashKey = Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name })
 
-    const cache = await client.get(assignedHashKey)
+    const cache = await client.get(JSON.stringify(assignedHashKey))
 
     if (cache) {
         const doc = JSON.parse(cache)
 
         return Array.isArray(doc)
-            ? doc.map(el => new this.model(doc))
+            ? doc.map(el => new this.model(el))
             : new this.model(doc)
     }
 
     const result = await exec.apply(this, arguments)
 
-    client.set(hashKey, JSON.stringify(result))
+    client.set(assignedHashKey, JSON.stringify(result))
 
     return result
 }
