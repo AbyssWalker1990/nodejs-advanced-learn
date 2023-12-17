b3bf936beda2eca8d31ff09c912519ad0746439e
Vova <bordiian.v@gmail.com>
2023-12-17 13:32:40 +0200

    incapsulate helpers logic in proxy

--- advancedNodeStart/tests/header.test.js
+++ advancedNodeStart/tests/header.test.js
@@ -1,18 +1,13 @@
-const puppeteer = require('puppeteer')
-const sessionFactory = require('./factories/sessionFactory')
-const userFactory = require('./factories/userFactory')
-let browser, page
+const Page = require('./helpers/Page')
+let page
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-        headless: false
-    })
-    page = await browser.newPage()
+    page = await Page.build()
     await page.goto('localhost:3000')
 })
 
 afterEach(async () => {
-    await browser.close()
+    await page.close()
 })
 
 test('The header has the correct name', async () => {
@@ -28,15 +23,7 @@ test('Log in button leads to gogle OAuth', async () => {
 })
 
 test('When sign in, shows logout button', async () => {
-    const user = await userFactory()
-
-    const { session, sig } = sessionFactory(user)
-
-    await page.setCookie({ name: 'session', value: session })
-    await page.setCookie({ name: 'session.sig', value: sig })
-    await page.goto('localhost:3000')
-    await page.waitFor('a[href="/auth/logout"]')
-
+    await page.login()
     const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
 
     expect(text).toEqual('Logout')
